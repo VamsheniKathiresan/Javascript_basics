6a88203dc630fa9d34006d435d51d48bd6b1f072
Vamsheni <vamsheni18@gmail>
2024-01-21 20:33:39 +0530

    first commit

---
+++ operators.js
@@ -0,0 +1,25 @@
+//assignment operator
+var a=10
+console.log("assignment operator output:",a)
+//arithmetic operator
+var a1=10
+var b1=10
+console.log("Arithmetic operator addition operation output:",a1+b1)
+//to find the type
+console.log(typeof(b1))
+console.log("Arithmetic operator subract operation output:",a1*b1)
+console.log("Arithmetic operator division operation output:",a1/b1)
+console.log("Arithmetic operator modulus operation output:",a1%b1)
+//logical operators  &&, ||,!
+console.log("Logical operator logical AND output:",a1&&b1)
+console.log("Logical operator logical OR output:",a1||b1)
+console.log("Logical operator logical NOT output:",!a1)
+console.log(10&&20)
+console.log(10&&30)
+console.log(10||20)
+console.log(0||20)
+console.log(0||20)
+console.log(100||20&&0)
+console.log((100||20)&&0)
+console.log(0&&(100||20))
+console.log(0&&100||20)
---
+++ p1.js
@@ -0,0 +1,10 @@
+a='a'
+console.log("1.",a)
+var b='var'
+b=10
+console.log("This variable belongs to 'var' type and it's value is ",b)
+let c='let'
+console.log("This variable belongs to 'let' type and it's value is",c)
+const d=100
+d=200
+console.log("This variable belongs to 'const' type and it's value is",d)
